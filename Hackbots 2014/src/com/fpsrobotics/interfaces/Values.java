a0be1562f4b2a6e45595abaa7d5b7d1375b840ad
rockbanddude <wildcat2468@yahoo.com>
2014-01-23 17:07:22 -0500

    Comments

--- Hackbots 2014/src/com/fpsrobotics/interfaces/Values.java
+++ Hackbots 2014/src/com/fpsrobotics/interfaces/Values.java
@@ -6,11 +6,12 @@ package com.fpsrobotics.interfaces;
 
 /**
  *
- * @author Benny
+ * @author Ben
  */
 public interface Values
 {
     //Motor Speeds
     double HALF_SPEED = 0.5;
     double NO_SPEED = 0.0;
+    //Put more important reusable values here
 }
