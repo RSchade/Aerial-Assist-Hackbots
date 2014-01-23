bb02616f1d6b034451fb3c5c1d702474addbe575
rockbanddude <wildcat2468@yahoo.com>
2014-01-23 16:55:34 -0500

    I fixed the interface!

--- Hackbots 2014/src/com/fpsrobotics/interfaces/Values.java
+++ Hackbots 2014/src/com/fpsrobotics/interfaces/Values.java
@@ -11,6 +11,6 @@ package com.fpsrobotics.interfaces;
 public interface Values
 {
     //Motor Speeds
-    final double HALF_SPEED = 0.5;
-    final double NO_SPEED = 0.0;
+    double HALF_SPEED = 0.5;
+    double NO_SPEED = 0.0;
 }
