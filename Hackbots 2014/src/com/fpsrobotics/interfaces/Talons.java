0e50a4cc4d5091c81d70ed51d538e54a805d1b5e
RSchade <rayschade@gmail.com>
2014-01-29 19:10:53 -0500

    Bugfix

--- Hackbots 2014/src/com/fpsrobotics/interfaces/Talons.java
+++ Hackbots 2014/src/com/fpsrobotics/interfaces/Talons.java
@@ -1,5 +1,6 @@
 package com.fpsrobotics.interfaces;
 
+import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
 
 /**
@@ -14,14 +15,8 @@ public interface Talons extends DeviceMap
     SpeedController leftDrive = new Talon(LEFT_DRIVE_MAP);
     SpeedController rightDrive = new Talon(RIGHT_DRIVE_MAP);
     
-<<<<<<< HEAD
     SpeedController spinnyLeftMotor = new Talon(LEFT_SPIN_MAP);
     SpeedController spinnyRightMotor = new Talon(RIGHT_SPIN_MAP);
-  
-=======
-    Talon spinnyLeftMotor = new Talon(LEFT_SPIN_MAP);
-    Talon spinnyRightMotor = new Talon(RIGHT_SPIN_MAP);
->>>>>>> e19c519264f301b9af14cd0b6de9f5b4e624862f
     
     SpeedController shooterTalon = new Talon(SHOOTER_TALON_MAP);
     // Create other Talons here
